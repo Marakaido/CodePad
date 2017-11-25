31bcbe6956a46dde6ed471f0116ed25d56f721df
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-11-25 11:50:29 +0200

    Move scene layout to fxml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,85 +1,33 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 import org.fxmisc.richtext.CodeArea;
 
 public class App extends Application {
 
+    public CodeArea codeArea;
+    public TabPane editorTabPane;
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override public void start(Stage primaryStage) {
-        executor = Executors.newSingleThreadExecutor();
-
-        Scene scene = buildScene();
-        scene.getStylesheets().add(App.class.getResource("style.css").toExternalForm());
-        primaryStage.setScene(scene);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(App.class.getResource("main.fxml"));
         primaryStage.setTitle("Codepad");
+        Scene scene = new Scene(root, 600, 400);
+        scene.getStylesheets().addAll(App.class.getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    @Override public void stop() {
-        executor.shutdown();
+    public void addNewTab(ActionEvent actionEvent) {
+        editorTabPane.getTabs().add(new Tab("new", new CodeArea()));
     }
-
-    private Scene buildScene() {
-        BorderPane root = new BorderPane();
-
-        MenuBar menu = buildMenu();
-        root.setTop(menu);
-
-        codeArea = new CodeArea();
-        Node center = buildCenter(codeArea);
-        root.setCenter(center);
-
-        Node footer = buildFooter();
-        root.setBottom(footer);
-
-        return new Scene(root, 600, 400);
-    }
-
-    private MenuBar buildMenu() {
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        Menu editMenu = new Menu("Edit");
-        Menu viewMenu = new Menu("View");
-        menuBar.getMenus().addAll(fileMenu, editMenu, viewMenu);
-        return menuBar;
-    }
-
-    private HBox buildFooter() {
-        HBox footer = new HBox();
-        footer.setAlignment(Pos.CENTER);
-        footer.getChildren().addAll(new Label("encoding: UTF-8"));
-        return footer;
-    }
-
-    private Node buildCenter(CodeArea codeArea) {
-        final AnchorPane root = new AnchorPane();
-        final TabPane tabPane = new TabPane(
-                new Tab("new", codeArea)
-        );
-
-        root.getChildren().addAll(tabPane);
-
-        AnchorPane.setTopAnchor(tabPane, 0.0);
-        AnchorPane.setBottomAnchor(tabPane, 0.0);
-        AnchorPane.setLeftAnchor(tabPane, 0.0);
-        AnchorPane.setRightAnchor(tabPane, 0.0);
-
-        return root;
-    }
-
-    private CodeArea codeArea;
-    private ExecutorService executor;
 }
