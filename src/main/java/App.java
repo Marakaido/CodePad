073d67473c72be105448df7f117a5d48c40ddf32
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-11-25 18:48:04 +0200

    Add open file command

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,24 +1,30 @@
 import editor.EditorArea;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.fxmisc.richtext.CodeArea;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class App extends Application {
-    public EditorArea codeArea;
-    public TabPane editorTabPane;
+    @FXML public TabPane editorTabPane;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    @Override public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
         Parent root = FXMLLoader.load(App.class.getResource("main.fxml"));
         primaryStage.setTitle("Codepad");
         Scene scene = new Scene(root, 600, 400);
@@ -30,6 +36,52 @@ public class App extends Application {
     }
 
     public void addNewTab(ActionEvent actionEvent) {
-        editorTabPane.getTabs().add(new Tab("new", new CodeArea()));
+        Tab tab = new Tab("new");
+        FileTab fileTab = new FileTab(tab, new EditorArea());
+        this.fileTabs.add(fileTab);
+        this.editorTabPane.getTabs().add(tab);
+    }
+
+    public void openFileInNewTab(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open file");
+        List<File> list = fileChooser.showOpenMultipleDialog(stage);
+        list.stream().filter(Objects::nonNull).forEach(file -> {
+            Tab tab = new Tab(file.getName());
+            FileTab fileTab = new FileTab(file, charset, tab, new EditorArea());
+            fileTabs.add(fileTab);
+            editorTabPane.getTabs().add(tab);
+        });
+    }
+
+    private List<FileTab> fileTabs = new ArrayList<>(10);
+    private Stage stage;
+    private Charset charset = Charset.forName("UTF-8");
+
+    public void createAndOpenFileInNewTab(ActionEvent actionEvent) {
+    }
+
+    public void openFileInNewWindow(ActionEvent actionEvent) {
+    }
+
+    public void saveFileInCurrentTab(ActionEvent actionEvent) {
+    }
+
+    public void saveFileInCurrentTabAndExit(ActionEvent actionEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+    }
+
+    public void copy(ActionEvent actionEvent) {
+    }
+
+    public void paste(ActionEvent actionEvent) {
+    }
+
+    public void cut(ActionEvent actionEvent) {
+    }
+
+    public void delete(ActionEvent actionEvent) {
     }
 }
---
+++ src/main/java/FileTab.java
@@ -0,0 +1,63 @@
+import editor.EditorArea;
+import javafx.scene.control.Tab;
+import org.fxmisc.richtext.CodeArea;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+public class FileTab {
+    public FileTab(final File file, final Charset charset, final Tab tab, final EditorArea editorArea) {
+        this(tab, editorArea);
+        this.setFile(file, charset);
+    }
+
+    public FileTab(final Tab tab, final EditorArea editorArea) {
+        this.tab = tab;
+        this.tab.setContent(editorArea);
+        this.editorArea = editorArea;
+    }
+
+    public File getFile() { return file; }
+    public void setFile(File file, Charset charset) {
+        try {
+            this.data = Files.readAllBytes(file.toPath());
+            this.file = file;
+            this.charset = charset;
+            this.updateText();
+        }
+        catch(IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public Charset getCharset() { return charset; }
+    public void setCharset(Charset charset) { this.charset = charset; }
+
+    public Tab getTab() { return tab; }
+    public void setTab(Tab tab) { this.tab = tab; }
+
+    public EditorArea getEditorArea() { return editorArea; }
+    public void setEditorArea(EditorArea editorArea) { this.editorArea = editorArea; }
+
+    public byte[] getData() { return data; }
+
+    public boolean hasFile() { return file != null; }
+
+    public CodeArea getEditor() { return this.editorArea; }
+
+    public void updateText() {
+        editorArea.replaceText(0, 0, new String(data, charset));
+    }
+    public void updateData() {
+        this.data = editorArea.getText().getBytes(charset);
+    }
+
+    private File file;
+    private Charset charset;
+    private Tab tab;
+    private EditorArea editorArea;
+    private byte[] data;
+}
